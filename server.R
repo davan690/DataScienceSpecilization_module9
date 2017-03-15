ee1d41a20bf675611255e400cea013e75f7deec0
jcwalmsley <jw.prof.cntrng@gmail.com>
2017-03-15 07:20:13 +0100

    updates

--- server.R
+++ server.R
@@ -12,6 +12,14 @@ shinyServer(function(input, output) {
         model1 <- lm(Ozone ~ Temp, data = acc)
         model2 <- lm(Ozone ~ Solar.R, data = acc)
 
+        sc <- summary(model1)$coefficients
+        ci1 <- sc[1,1]+c(-1,1)*qt(.975,df=model1$df)*sc[1,2]
+        ci1
+
+        sc <- summary(model2)$coefficients
+        ci2 <- sc[1,1]+c(-1,1)*qt(.975,df=model2$df)*sc[1,2]
+        ci2
+
         model1pred <- reactive({
                 TempInput <- input$sliderTemp
                 predict(model1, newdata = data.frame(Temp = TempInput))
@@ -21,31 +29,42 @@ shinyServer(function(input, output) {
                 TempInput <- input$sliderTemp
                 SolarInput <- input$sliderSolar.R
                 predict(model2, newdata = data.frame(Temp = TempInput, Solar.R = SolarInput))
-})
-        output$plot <- renderPlot({
+
+        })
+        output$plot1 <- renderPlot({
                 TempInput <- input$sliderTemp
-                SolarInput <- input$sliderSolar.R
-                plot(acc$Temp, acc$Ozone, xlab = "Temp",
+                plot(acc$Temp, acc$Ozone, xlab = "Temperature",
                      ylab = "Ozone in PPM", bty = "n", pch = 16,
-                     xlim = c(40, 150), ylim = c(0, 170))
+                     xlim = c(-10, 120), ylim = c(0, 170))
                 if(input$showModel1){
                         abline(model1, col = "red", lwd = 2)
                 }
+                legend(-10, 120, c("Model 1 Prediction"), pch = 16,
+                       col = "red", bty = "n", cex = 1.2)
+                points(TempInput, model1pred(), col = "red", pch = 16, cex = 2)
+
+
+
+                })
+        output$plot2 <- renderPlot({
+                SolarInput <- input$sliderSolar.R
+                plot(acc$Solar.R, acc$Ozone, xlab = "Solar.R",
+                     ylab = "Ozone in PPM", bty = "n", pch = 16,
+                     xlim = c(0, 350), ylim = c(0, 170))
                 if(input$showModel2){
                         abline(model2, col = "blue", lwd = 2)
                 }
-                legend(40, 150, c("Model 1 Prediction", "Model 2 Prediction"), pch = 16,
-                       col = c("red", "blue"), bty = "n", cex = 1.2)
-                points(TempInput, model1pred(), col = "red", pch = 16, cex = 2)
+                legend(40, 350, c("Model 2 Prediction"), pch = 16,
+                       col = "blue", bty = "n", cex = 1.2)
                 points(SolarInput, model2pred(), col = "blue", pch = 16, cex = 2)
         })
-
         output$pred1 <- renderText({
                 model1pred()
-        })
 
+        })
         output$pred2 <- renderText({
                 model2pred()
+
         })
 })
 
--- ui.R
+++ ui.R
@@ -1,20 +1,25 @@
 library(shiny)
 shinyUI(fluidPage(
-        titlePanel("Predict Ozone from Temp in Model 1 or a Temp & Solar.R combination in Model 2"),
+        titlePanel(title = h1("Predicting Ozone PPM based on Temperature or Solar.R", align = "center")),
+        br(),
         sidebarLayout(
                 sidebarPanel(
-                        sliderInput("sliderTemp", "For prediction Model 1 input the desired Temperature?", 60, 100, value = 80),
-                        sliderInput("sliderSolar.R", "For prediction Model 2 input the estmated Solar.R?", 40, 150, value = 100),
+                        sliderInput("sliderTemp", "For prediction Model 1 input the desired Temperature in degrees F?", 50, 120, value = 60),
+                        sliderInput("sliderSolar.R", "For prediction Model 2 input the estmated Solar.R in W/m^2?", 5, 350, value = 100),
                         checkboxInput("showModel1", "Show/Hide model1", value = TRUE),
                         checkboxInput("showModel2", "Show/Hide model2", value = TRUE),
                         submitButton("Submit")
                 ),
                 mainPanel(
-                        plotOutput("plot"),
-                        h3("Predicted Ozone from model1:"),
+                        br(),
+                        titlePanel(title=h3("Prediction of Ozone from Temperature Model 1", align = "center")),
+                        plotOutput("plot1"),
                         textOutput("pred1"),
-                        h3("Predicted Ozone from model2:"),
-                        textOutput("pred2")
+                        br(),
+                        titlePanel(title=h3("Prediction of Ozone from Solar.R Model 2", align = "center")),
+                        plotOutput("plot2"),
+                        textOutput("pred2"),
+                        br()
                 ),
         )
 ))
