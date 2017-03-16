6b42a14581ee196f3047712e434a9a0648a9e0a9
jcwalmsley <jw.prof.cntrng@gmail.com>
2017-03-17 00:07:29 +0100

    updates

--- server.R
+++ server.R
@@ -3,8 +3,7 @@ library(dplyr)
 data(airquality)
 library(ggplot2)
 
-
-acc <- airquality[complete.cases(airquality),]
+acc <<- airquality[complete.cases(airquality),]
 str(acc)
 
 shinyServer(function(input, output) {
@@ -12,60 +11,66 @@ shinyServer(function(input, output) {
         model1 <- lm(Ozone ~ Temp, data = acc)
         model2 <- lm(Ozone ~ Solar.R, data = acc)
 
-        sc <- summary(model1)$coefficients
-        ci1 <- sc[1,1]+c(-1,1)*qt(.975,df=model1$df)*sc[1,2]
-        ci1
-
-        sc <- summary(model2)$coefficients
-        ci2 <- sc[1,1]+c(-1,1)*qt(.975,df=model2$df)*sc[1,2]
-        ci2
-
-        model1pred <- reactive({
-                TempInput <- input$sliderTemp
-                predict(model1, newdata = data.frame(Temp = TempInput))
-        })
-
-        model2pred <- reactive({
-                TempInput <- input$sliderTemp
-                SolarInput <- input$sliderSolar.R
-                predict(model2, newdata = data.frame(Temp = TempInput, Solar.R = SolarInput))
-
-        })
+        output$data <- renderTable({
+                colm <- as.numeric(input$var)
+                head(acc[colm],10)
+                })
+        output$sum <- renderPrint({
+                summary(acc)
+                })
+        output$str <- renderPrint({
+                str(acc)
+                })
         output$plot1 <- renderPlot({
                 TempInput <- input$sliderTemp
-                plot(acc$Temp, acc$Ozone, xlab = "Temperature",
-                     ylab = "Ozone in PPM", bty = "n", pch = 16,
-                     xlim = c(-10, 120), ylim = c(0, 170))
-                if(input$showModel1){
+                plot(acc$Temp, acc$Ozone, xlab = "Temperature in degrees F",
+                        ylab = "Ozone ppm", bty = "n", pch = 16,
+                        xlim = c(61, 110), ylim = c(0, 170))
                         abline(model1, col = "red", lwd = 2)
-                }
-                legend(-10, 120, c("Model 1 Prediction"), pch = 16,
+                legend(60,160, c("Ozone prediction"), pch = 16,
                        col = "red", bty = "n", cex = 1.2)
                 points(TempInput, model1pred(), col = "red", pch = 16, cex = 2)
-
-
-
+                })
+        output$pred1 <- renderText({
+                round(model1pred(),2)
+                })
+        output$ci1 <- renderPrint({
+                confint(model1)
                 })
         output$plot2 <- renderPlot({
                 SolarInput <- input$sliderSolar.R
-                plot(acc$Solar.R, acc$Ozone, xlab = "Solar.R",
-                     ylab = "Ozone in PPM", bty = "n", pch = 16,
-                     xlim = c(0, 350), ylim = c(0, 170))
-                if(input$showModel2){
-                        abline(model2, col = "blue", lwd = 2)
-                }
-                legend(40, 350, c("Model 2 Prediction"), pch = 16,
+                plot(acc$Solar.R, acc$Ozone, xlab = "Solar.R in W/m^2",
+                        ylab = "Ozone ppm", bty = "n", pch = 16,
+                        xlim = c(5, 350), ylim = c(0, 170),
+                        abline(model2, col = "blue", lwd = 2))
+                legend(5,160, c("Ozone prediction"), pch = 15,
                        col = "blue", bty = "n", cex = 1.2)
-                points(SolarInput, model2pred(), col = "blue", pch = 16, cex = 2)
-        })
-        output$pred1 <- renderText({
-                model1pred()
-
-        })
+                points(SolarInput, model2pred(), col = "blue", pch = 15, cex = 2)
+                })
         output$pred2 <- renderText({
-                model2pred()
-
-        })
+                round(model2pred(),2)
+                })
+        output$ci2 <- renderPrint({
+                confint(model2)
+                })
+        model1pred <- reactive({
+                TempInput <- input$sliderTemp
+                predict(model1, newdata = data.frame(Temp = TempInput))
+                })
+        model2pred <- reactive({
+                SolarInput <- input$sliderSolar.R
+                predict(model2, newdata = data.frame(Solar.R = SolarInput))
+                })
+        output$counter <-
+                renderText({
+                        if (!file.exists("counter.Rdata"))
+                                counter <- 0
+                        else
+                                load(file = "counter.Rdata")
+                        counter <- counter + 1
+                        save(counter, file="counter.Rdata")
+                        paste0("Hits: ", counter)
+                })
 })
 
 
--- ui.R
+++ ui.R
@@ -1,24 +1,35 @@
 library(shiny)
 shinyUI(fluidPage(
-        titlePanel(title = h1("Predicting Ozone PPM based on Temperature or Solar.R", align = "center")),
+        titlePanel(title = h1("Predicting levels of ppm Ozone based on Temperature or Solar Radiation inputs", align = "center")),
         br(),
         sidebarLayout(
                 sidebarPanel(
-                        sliderInput("sliderTemp", "For prediction Model 1 input the desired Temperature in degrees F?", 50, 120, value = 60),
-                        sliderInput("sliderSolar.R", "For prediction Model 2 input the estmated Solar.R in W/m^2?", 5, 350, value = 100),
-                        checkboxInput("showModel1", "Show/Hide model1", value = TRUE),
-                        checkboxInput("showModel2", "Show/Hide model2", value = TRUE),
-                        submitButton("Submit")
+                        selectInput("var", "1. To view the the first ten rows of observations of a given variable in the acc subset of the airquality data set, first select the Data Tab to the right, then select a variable name from the drop down menu below, finally click the submit button located at the bottom of this panel to view results.", choices = c("Temp"=1,"Solar.R"=2,"Ozone"=3,"Wind"=4,"Month"=5,"Day"=6)),
+                        br(),
+                        sliderInput("sliderTemp", "2. To get a prediction for ppm of Ozone using Model 1, first select the Model 1 Plot Tab to the right, next select the desired Temperature input on the slider below, next click the submit button at the bottom of this panel to view plot results, to see the Ozone prediction select the Modlel 1 Pred, Tab, and to see the confidence interval of Model 1select the Model 1 CI, Tab.", 61, 110, value = 71),
+                        br(),
+                        sliderInput("sliderSolar.R", "3. To get a prediction for ppm of Ozone using Model 2, first select the Model 2 Plot Tab to the right, next select the desired Solar.R  input on the slider below, next click the submit button below to view the plot results, to see the Ozone prediction from Model 2 select the Model 2 Pred, Tab, and to see the confidence interval of Model 2 select the Model 2 CI, Tab.", 5, 350, value = 75),
+                        br(),
+                        h5("To view the results of the summary() or structure() functions on the acc data subset, select the respective Summary or Sturcture Tabs"),
+                        br(),
+                        submitButton("Submit"),
+                        br(),
+                        h5(textOutput("counter")),
+                        tags$style("body{background-color:tan; color:blue}"),
+                        br()
                 ),
                 mainPanel(
-                        br(),
-                        titlePanel(title=h3("Prediction of Ozone from Temperature Model 1", align = "center")),
-                        plotOutput("plot1"),
-                        textOutput("pred1"),
-                        br(),
-                        titlePanel(title=h3("Prediction of Ozone from Solar.R Model 2", align = "center")),
-                        plotOutput("plot2"),
-                        textOutput("pred2"),
+                        tabsetPanel(type="tab",
+                                tabPanel("Data", tableOutput("data")),
+                                tabPanel("Summary", verbatimTextOutput("sum")),
+                                tabPanel("Structure", verbatimTextOutput("str")),
+                                tabPanel("Model 1 Plot", plotOutput("plot1")),
+                                tabPanel("Model 1 Pred ", verbatimTextOutput("pred1")),
+                                tabPanel("Model 1 CI", verbatimTextOutput("ci1")),
+                                tabPanel("Model 2 Plot", plotOutput("plot2")),
+                                tabPanel("Model 2 Pred", verbatimTextOutput("pred2")),
+                                tabPanel("Model 2 CI" , verbatimTextOutput("ci2"))
+                                    ),
                         br()
                 ),
         )
